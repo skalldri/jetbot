c19f96846e1417b15eaed33aa96f503ddeef4093
Stuart Alldritt <s.k.alldritt@gmail.com>
2021-09-13 00:16:38 -0700

    Make build scripts inherit environment from sudo

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -1,8 +1,13 @@
 cp /etc/apt/trusted.gpg.d/jetson-ota-public.asc ../.. # copy to jetbot root
 
-sudo docker build \
+OUTPUT_IMAGE=$JETBOT_DOCKER_REMOTE/jetbot:base-$JETBOT_VERSION-$L4T_VERSION
+
+echo "Building Jetbot Base Image: $OUTPUT_IMAGE"
+echo "Using Base Docker Image: $JETBOT_BASE_IMAGE"
+
+sudo -E docker build \
     --build-arg BASE_IMAGE=$JETBOT_BASE_IMAGE \
-    -t $JETBOT_DOCKER_REMOTE/jetbot:base-$JETBOT_VERSION-$L4T_VERSION \
+    -t $OUTPUT_IMAGE \
     -f Dockerfile \
     ../..  # jetbot repo root as context
 
